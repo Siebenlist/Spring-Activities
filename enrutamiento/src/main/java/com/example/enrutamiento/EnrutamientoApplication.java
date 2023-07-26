d855cd67c1cbca291ea3a864bc3aa0c89431cb1e
Siebenlist <siebenlist.sr@gmail.com>
2023-07-26 18:04:33 -0300

    Added new project

---
+++ enrutamiento/src/main/java/com/example/enrutamiento/EnrutamientoApplication.java
@@ -0,0 +1,13 @@
+package com.example.enrutamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class EnrutamientoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EnrutamientoApplication.class, args);
+	}
+}
